20b6a4eb50d527361dffa305604336cb802bfd6e
Macdonald Onwuma <macdonaldonwuma@gmail.com>
2022-09-01 07:03:01 +0000

    import modules corrections

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
 if __name__ == "__main__":
-    discovr()
+    import hidden_4
+    for a in dir(hidden_4):
+        if a[0] != "_" and a[1] != "_":
+            print("{}".format(a))
